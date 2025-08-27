c7b7ab289e9682fc093fa94b9ad2cfa18976544f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-27 23:21:44 +0530

    Add files via upload

---
+++ Practise25.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Practise25 {
+                         public static void main(String[]abc)
+                         {
+                            Scanner sc=new Scanner(System.in);
+                            int num;
+                            System.out.println("Enter number");
+                            num=sc.nextInt();
+                            if(num>7&&num==9||num<=8)
+                            {
+                              System.out.println("Inside if block");
+                            }
+                            else 
+                            {
+                               System.out.println("Inside else Block");
+                            }
+                         
+                         }
+                         
+}

6b439d99ce963913cdc1f657775b0263d538bccc
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-28 22:41:40 +0530

    Add files via upload

---
+++ Practice31.java
@@ -0,0 +1,18 @@
+public class Practice31 
+{
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+
+    public static void main(String[] args) {
+        Practice31 sol = new Practice31();
+
+        
+        System.out.println(sol.theMaximumAchievableX(4, 1));  // Output: 6
+        
+    }
+}
+
+
+                         
+

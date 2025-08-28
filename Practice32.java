cd6af5080aa399017973146504a7f91626b19912
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-28 22:44:29 +0530

    Add files via upload

---
+++ Practice32.java
@@ -0,0 +1,18 @@
+public class Practice32 {
+
+
+    public String toLowerCase(String s) {
+        s = s.toLowerCase();
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Practice32 sol = new Practice32();
+
+        
+        System.out.println(sol.toLowerCase("Hello"));      
+    }
+}
+
+                         
+

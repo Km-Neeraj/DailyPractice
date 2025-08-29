73de637a2df4db00d8a3f9e2f3a98cdfe59dd4b6
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-29 23:55:14 +0530

    Add files via upload

---
+++ Practice34.java
@@ -0,0 +1,9 @@
+public class Practice34 {
+                         public static void main(String[]abc)
+                         {
+                             int a=100;
+                             int res= a-- + --a - a-- /a++ + ++a;
+                             System.out.println(res);
+                         }
+                         
+}

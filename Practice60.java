95486eb1382a15ac06bca3952490ce9ba68e8a50
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-02 23:15:34 +0530

    Add files via upload

---
+++ Practice60.java
@@ -0,0 +1,30 @@
+public class Practice60 {
+                         public static void main(String[]abc)
+                         {
+                              for(int i=1;i<=5;i++)
+                              {
+                                  for(int j=i;j<5;j++)
+                                  {
+                                      System.out.print(" ");
+                                  }
+                                  for(int k=1;k<=2*i-1;k++)
+                                  {
+                                      System.out.print("*"+" ");
+                                  }
+                                  System.out.println();
+                              }
+                              for(int i=4;i>=1;i--)
+                              {
+                                  for(int j=i;j<5;j++)
+                                  {
+                                      System.out.print(" ");
+                                  }
+                                  for(int k=1;k<=2*i-1;k++)
+                                  {
+                                      System.out.print("*"+" ");
+                                  }
+                                  System.out.println();
+                              }
+                         }
+                         
+}

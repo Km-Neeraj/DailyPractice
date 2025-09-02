031e75787637c941f23ea48934775cb7db2892ee
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-02 23:08:03 +0530

    Add files via upload

---
+++ Practice59.java
@@ -0,0 +1,18 @@
+public class Practice59 {
+                         public static void main(String[]abc)
+                         {
+                             for(int i=1;i<=3;i++)
+                             {
+                                                  int n=i;
+                               for(int j=i;j<=3;j++)
+                               {
+                                  
+                                  System.out.print(n+" ");
+                                  n++;
+                               }
+                               System.out.println();
+                             }
+
+                         }
+                         
+}

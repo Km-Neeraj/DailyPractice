7bfecda2344b07e9289325ab8932666660690a21
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-02 23:03:36 +0530

    Add files via upload

---
+++ Practice58.java
@@ -0,0 +1,9 @@
+public class Practice58 {
+                         public static void main(String[]abc)
+                         {
+                             int x=10;
+                             int res= ++x + x++ + --x + x-- * x;
+                             System.out.println("Result = "+res);
+                         }
+                         
+}

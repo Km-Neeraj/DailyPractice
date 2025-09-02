aaaa710a8bf22411495a97681cf79a63f8b56f8c
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-02 22:58:15 +0530

    Add files via upload

---
+++ Practice57.java
@@ -0,0 +1,14 @@
+public class Practice57 {
+                         public static void main(String[]abc)
+                         {
+                            int x=10;
+                            {
+                              int y=20;
+                              System.out.println(x+" "+y);
+                            }
+                           // System.out.println(x+" "+y); Error 
+
+
+                         }
+                         
+}

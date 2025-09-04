f7cd4136912bf11b6a38386e6718fb33ec6b7a75
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-04 22:48:31 +0530

    Add files via upload

---
+++ Move.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution 
+{
+    static int[] moveZeros(int[]arr,int n)
+    {
+        int[]res=new int[n];
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+           if(arr[i]!=0)
+           {
+                         res[j++]=arr[i];
+           }
+        }
+        return res;
+    }
+}
+public class Move {
+                         public static void main(String[]an)
+                         {
+                            int n;
+                            Scanner sc=new Scanner(System.in);
+                            n=sc.nextInt();
+                            int[]arr=new int[n];
+                            for(int i=0;i<n;i++)
+                            {
+                                arr[i]=sc.nextInt();
+
+                            }
+                            int[]res=Solution.moveZeros(arr,n);
+                            for(int i=0;i<n;i++)
+                            {
+                               System.out.print(res[i]+" ");
+                            }
+                            sc.close();
+                         }
+                         
+}

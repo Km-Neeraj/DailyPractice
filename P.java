d19f1130d74b294b5c8e76de8b6127ee1161da16
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-04 22:48:49 +0530

    Add files via upload

---
+++ P.java
@@ -0,0 +1,16 @@
+public class P {
+                         public static void main(String[]abc)
+                         {
+                             String s1="ana";
+                             char[]ch=s1.toCharArray();
+                             int n=ch.length;
+                             char[]res=new char[n];
+                             int j=0;
+                             for(int i=n-1;i>=0;i--)
+                             {
+                                                  res[j++]=ch[i];
+                             }
+                             if()
+                         }
+                         
+}

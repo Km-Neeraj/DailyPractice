380472e94a1c5ab00288efcdcfee59e51ba0a057
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-08 21:44:22 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,11 @@
+public class Fact {
+                         public static void main(String[]abc)
+                         {
+                            int a=10;
+                            int b=20;
+                            int res=a+++--b-a++;
+                            System.out.println(res);
+
+                         }
+                         
+}

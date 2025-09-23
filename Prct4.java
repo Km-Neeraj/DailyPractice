19ae6bd55bf523c754ed4875d0d21c2d4657eca7
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-24 00:00:29 +0530

    Add files via upload

---
+++ Prct4.java
@@ -0,0 +1,11 @@
+public class Prct4 {
+                         public static void main(String[] args) {
+
+                           int m=20;
+                           int n=10;
+                           int res=(m&n)*(m>>3)/(n<<m);
+                           System.out.println(res);
+                                                  
+                         }
+                         
+}

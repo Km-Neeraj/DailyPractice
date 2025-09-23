e280a7df73ade57fa0a19701c46dfba987ff3a3b
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-23 23:53:24 +0530

    Add files via upload

---
+++ prct.java
@@ -0,0 +1,11 @@
+public class prct {
+                         public static void main(String[]abc)
+                         {
+                            int a=10;
+                            int b=20;
+                            int res=(a|b)+(a>>2)*(b>>a);
+                            System.out.println(res);
+                            
+                         }
+                         
+}

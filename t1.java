340fa58d73effde26a7b0bc8183a9d58d2442855
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-25 23:56:29 +0530

    Add files via upload

---
+++ t1.java
@@ -0,0 +1,14 @@
+public class t1 {
+                         public static void main(String[]abc)
+                         {
+                             for(int i=1;i<=5;i++)
+                             {
+                                for(int j=i;j<=5;j++)
+                                {
+                                   System.out.print("*"+" ");
+                                }
+                                System.out.println();
+                             }
+                         }
+                         
+}

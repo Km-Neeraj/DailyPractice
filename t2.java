a261b22d1df02e9e5b808e2da93886aa4b2c9367
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-25 23:57:58 +0530

    Add files via upload

---
+++ t2.java
@@ -0,0 +1,10 @@
+public class t2 {
+                         public static void main(String[] args) {
+                                                  
+
+                                                  int a=10;
+                                                  int res=(a+10)+(a>>3)+(a<<2);
+                                                  System.out.println(res);
+                         }
+                         
+}

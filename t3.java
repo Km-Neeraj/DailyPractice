b98efa2c83eac4af3d77330bc88de317b43f6454
Neeraj Yadav <neerajydv1113@gmail.com>
2025-09-25 23:59:26 +0530

    Add files via upload

---
+++ t3.java
@@ -0,0 +1,14 @@
+public class t3 {
+                         public static void main(String[] args) {
+
+                           for(int i=1;i<=50;i++)
+                           {
+                              if(i%2==0)
+                              {
+                                 System.out.print(i+" ");
+                              }
+                           }
+                                                  
+                         }
+                         
+}

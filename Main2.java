514590999efba7a2ec29f5217e6b35a149864886
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-01 23:55:06 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+
+                         public static void main(String[] args) {
+                                                  
+                             int b=100;
+
+                             int res= --b - --b - b-- - --b;
+                             System.out.println(res);
+                         }
+                         
+}

289766baeaa76c7331f3e1b0ba82e38f41e32232
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-01 23:57:51 +0530

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,9 @@
+public class Main4 {
+                         public static void main(String[] args) {
+                                                  
+                            int num=100;
+                            int res=num++ + --num - num/20+num/10+num*20-num--;
+                            System.out.println(res);
+                         }
+                         
+}

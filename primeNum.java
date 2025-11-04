2775d2c1c897d7ba3b3521617b8d72c221b5563c
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-04 23:57:38 +0530

    Add files via upload

---
+++ primeNum.java
@@ -0,0 +1,23 @@
+public class primeNum {
+                         public static void main(String[] args) {
+                           
+                             int range=30;
+                             for(int i=1;i<=range;i++)
+                             {
+                                 int count=0;
+                                 for(int j=1;j<=i;j++)
+                                 {
+                                    if(i%j==0)
+                                    {
+                                       count++;
+                                    }
+                                 }
+                                 if(count==2)
+                                 {
+                                    System.out.print(i+" ");
+                                 }
+                             }
+                             
+                         }
+                         
+}

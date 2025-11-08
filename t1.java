b28994a3608f8241834291730db423c21b563463
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-08 18:30:02 +0530

    Add files via upload

--- t1.java
+++ t1.java
@@ -1,14 +1,47 @@
+import java.util.*;
+class Solution 
+{
+  static boolean result(int[]arr,int n,int target)
+  {
+      Arrays.sort(arr);
+      int left=0;
+      int right=n-1;
+      while(left<right)
+      {
+        int sum=arr[left]+arr[right];
+        if(sum==target)
+        {
+          return true;  
+        }
+        else if(sum<target)
+        left++;
+        else 
+        right--;
+
+      }
+      return false;
+  }
+}
 public class t1 {
-                         public static void main(String[]abc)
-                         {
-                             for(int i=1;i<=5;i++)
-                             {
-                                for(int j=i;j<=5;j++)
-                                {
-                                   System.out.print("*"+" ");
-                                }
-                                System.out.println();
-                             }
+       
+                         public static void main(String[] args) {
+                                                  
+                            int n;
+                            Scanner sc=new Scanner(System.in);
+                            n=sc.nextInt();
+                            int[]arr=new int[n];
+                            for(int i=0;i<n;i++)
+                            {
+                               arr[i]=sc.nextInt();
+                            }
+                            System.out.println("Enter target ");
+                            int target=sc.nextInt();
+                            boolean res=Solution.result(arr,n,target);
+                            if(res)
+                            System.out.println("Yes target sum exist");
+                            else 
+                            System.out.println("No target sum is not exist");
+                            sc.close();
                          }
                          
 }

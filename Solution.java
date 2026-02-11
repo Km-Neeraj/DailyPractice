cfcb3f4889b6e607e43742c41e00a926d219c855
Neeraj Yadav <neerajydv1113@gmail.com>
2026-02-11 18:08:50 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,15 @@
+public class Solution 
+{
+                         static int a=20;
+                         static{
+                                                  System.out.println("Static block executed");
+                         }
+
+
+public static void main(String[]abc)
+
+{
+                         System.out.println("Value of static memeber a = "+a);
+}
+}
+

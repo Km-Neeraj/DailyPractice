a03d7e16d2f036b2884c281ff31fd035b8c920fa
Neeraj Yadav <neerajydv1113@gmail.com>
2026-02-13 17:04:07 +0530

    Add files via upload

---
+++ methodOverriding.java
@@ -0,0 +1,23 @@
+class Animal
+{
+                         void sound()
+                         {
+                            System.out.println("Animal makes sound");
+                         }
+}
+class Dog extends Animal{
+                          
+                           void sound()
+                           {
+                                                  System.out.println("Dog Barks");
+                           }
+}
+public class methodOverriding {
+                         public static void main(String[] args) {
+
+                                                  Animal ob=new Dog();
+                                                  ob.sound();
+                                                  
+                         }
+                         
+}

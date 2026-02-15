e8897a9db283288036afdc11b2de26a1d3a776cf
Neeraj Yadav <neerajydv1113@gmail.com>
2026-02-15 10:29:21 +0530

    Add files via upload

---
+++ Anyms3.java
@@ -0,0 +1,23 @@
+//Implementation of a normal class  by anonymous class
+ class Test
+{
+                         void fun()
+                         {
+                                                  System.out.println("Welcome to Test Class");
+                         }
+                         
+}
+
+public class Anyms3 {
+                         public static void main(String[] args) {
+                                                  Test t=new Test() {
+                                                                           
+                                                                                                   
+                                                                           
+                                                  };
+                                                  t.fun();
+                                                  
+                         }
+                         
+}
+
